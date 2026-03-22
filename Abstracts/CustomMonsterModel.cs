61a8e94ffb9c3a73071bc0dc1bb84c2571b36007
Syra 3 <25936043+SyraLessThanThree@users.noreply.github.com>
2026-03-22 07:38:20 +0100

    Barebones CustomMonsterModel.cs and CustomPetModel.cs

---
+++ Abstracts/CustomMonsterModel.cs
@@ -0,0 +1,37 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Animation;
+using MegaCrit.Sts2.Core.Bindings.MegaSpine;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+
+namespace BaseLib.Abstracts;
+
+public abstract class CustomMonsterModel : MonsterModel, ICustomModel
+{
+    /// <summary>
+    /// Override this or place your scene at res://scenes/creature_visuals/class_name.tscn
+    /// </summary>
+    public virtual string? CustomVisualPath => null;
+    
+    
+    /// <summary>
+    /// By default, will convert a scene containing the necessary nodes into a NCreatureVisuals even if it is not one.
+    /// </summary>
+    /// <returns></returns>
+    public virtual NCreatureVisuals? CreateCustomVisuals()
+    {
+        if (CustomVisualPath == null) return null;
+        return GodotUtils.CreatureVisualsFromScene(CustomVisualPath);
+    }
+    
+    
+    /// <summary>
+    /// Override and return a CreatureAnimator if you need to set up states that differ from the default for your character.
+    /// Using <seealso cref="SetupAnimationState"/> is suggested.
+    /// </summary>
+    /// <returns></returns>
+    public virtual CreatureAnimator? SetupCustomAnimationStates(MegaSprite controller)
+    {
+        return null;
+    }
+}
---
+++ Abstracts/CustomPetModel.cs
@@ -0,0 +1,39 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Animation;
+using MegaCrit.Sts2.Core.Bindings.MegaSpine;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.MonsterMoves.MonsterMoveStateMachine;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+
+namespace BaseLib.Abstracts;
+
+public class CustomPetModel: CustomMonsterModel ,ICustomModel{
+    /// <summary>
+    /// Override this or place your scene at res://scenes/creature_visuals/class_name.tscn
+    /// </summary>
+    public new virtual string? CustomVisualPath => null;
+    
+    public override int MinInitialHp => 9999;
+
+    public override int MaxInitialHp => 9999;
+
+    public override bool IsHealthBarVisible => false;
+    
+    /// <summary>
+    /// By default, will convert a scene containing the necessary nodes into a NCreatureVisuals even if it is not one.
+    /// </summary>
+    /// <returns></returns>
+    public virtual NCreatureVisuals? CreateCustomVisuals()
+    {
+        if (CustomVisualPath == null) return null;
+        return GodotUtils.CreatureVisualsFromScene(CustomVisualPath);
+    }
+
+    protected override MonsterMoveStateMachine GenerateMoveStateMachine()
+    {
+        MoveState nothingState = new MoveState("NOTHING_MOVE", (IReadOnlyList<Creature> _) => Task.CompletedTask);
+        nothingState.FollowUpState = nothingState;
+        return new MonsterMoveStateMachine([nothingState], nothingState);
+    }
+}
